b6929e2d8b83c4f04c745e980358c475d0293ffe
aaryyann <arya000045@gmail.com>
2025-04-19 05:39:31 +0530

    files initialized

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+
+import NavbarDemo from '@/components/main/Navbar'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='bg-gray-800'>
+      <NavbarDemo/>
+    </div>
+  )
+}
+
+export default page
